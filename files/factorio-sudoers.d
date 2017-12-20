7eddf8dcc3bac265b3e65f7ae46fac5e513226ef
Pavel Akhmetshin <linko22@gmail.com>
2017-12-20 10:19:48 +0300

    Initial commit

---
+++ files/factorio-sudoers.d
@@ -0,0 +1,3 @@
+factorio        ALL=NOPASSWD: /bin/systemctl restart factorio.service
+factorio        ALL=NOPASSWD: /bin/systemctl start factorio.service
+factorio        ALL=NOPASSWD: /bin/systemctl stop factorio.service
